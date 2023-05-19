4f9849c9b9433f8072836b27cda5bbee71140a13
Eliezir <eliezir2004@gmail.com>
2023-05-18 22:07:03 -0300

    configurando o mongodb e mongoose

--- app.js
+++ app.js
@@ -7,10 +7,16 @@ let expressSession = require("express-session"); //#
 let methodOverride = require("method-override");
 let error = require("./middlewares/error"); // #!
 
+const KEY = 'pweb.agenda.sid'; //#!
+const SECRET = 'super_secreto!'; //#!
 
 
-var app = express();
+let cookie = cookieParser(SECRET); //#!
+let store = new expressSession.MemoryStore(); //#!
+let mongoose = require('mongoose'); //#!
 
+
+const app = express();
 
 
 
@@ -28,17 +34,17 @@ load("models").then("controllers").then("routes").into(app);
 app.use(error.notFound);
 app.use(error.serverError);
 
+//Local host não funciona com o node 17+
+const mongoDB = "mongodb://0.0.0.0:27017/CinemaBlessed";
+mongoose.connect(mongoDB, { useNewUrlParser: true });
 
-
-
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 app.use(express.static(path.join(__dirname, '/public/')));
 
-load('models')
-.then('controllers')
-.then('routes')
-.into(app);
+
 
 app.listen(3000,function(){
   console.log('http://localhost:3000/')
