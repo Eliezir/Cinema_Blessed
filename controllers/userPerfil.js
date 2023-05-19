adea62aa960da9fc2a8c69fd26b08c4173efd302
Eliezir <eliezir2004@gmail.com>
2023-05-18 22:10:45 -0300

    Alteração Mongo - User do banco e  follow/unf

--- controllers/userPerfil.js
+++ controllers/userPerfil.js
@@ -1,29 +1,31 @@
 module.exports = function(app){
+    var Usuario = app.models.usuario; //# <<
     var userPerfilController = {
-        index: function(req,res){
+        index: async function(req,res){
             let usuario = req.session.usuario;
             let userName = req.params.user;
-            let users = req.session.users
-            let userIndex = users.findIndex(user => user.nome === userName)
-            let comentarios = users[userIndex].comentarios
-            let watchlist = users[userIndex].watchlist
-            let params = {usuario: usuario, user:users[userIndex], comentarios: comentarios, watchlist: watchlist}
+            const userProfile = await Usuario.findOne({ user: userName  });
+            let comentarios = userProfile.comentarios
+            let watchlist = userProfile.watchlist
+            let params = {usuario: usuario, user:userProfile, comentarios: comentarios, watchlist: watchlist}
        
             res.render("usersPerfil/userIndex.ejs",params)
         },
 
-        follow: function(req,res){
+        follow: async function(req,res){
             usuario = req.session.usuario
             let userName = req.params.user;
-            usuario.seguindo.push(userName)
+            usuario.following.push(userName)
             res.redirect(req.get('referer'))
+            await Usuario.updateOne({ _id: usuario._id }, usuario);
         },
-        unfollow: function(req,res){
+        unfollow: async function(req,res){
             usuario = req.session.usuario
             let userName = req.params.user;
-            let index = usuario.seguindo.indexOf(userName);
-            usuario.seguindo.splice(index, 1);
+            let index = usuario.following.indexOf(userName);
+            usuario.following.splice(index, 1);
             res.redirect(req.get('referer'))
+            await Usuario.updateOne({ _id: usuario._id }, usuario);
         }
     }
     return userPerfilController;
