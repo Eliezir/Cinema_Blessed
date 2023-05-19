cd87c58162dcd8986b8ebb5ebf45d56ab27cdd62
Eliezir <eliezir2004@gmail.com>
2023-05-18 22:10:54 -0300

    limpando o código

--- views/perfil/teste.js
+++
@@ -1,9 +0,0 @@
-let amigos = [];
-usuario.seguindo.forEach(function(userName){
-  let amigo = users[users.findIndex(user => user.user === userName)]
-    amigos.push({
-      nome:amigo.nome,
-      match:0,
-      comentarios:amigo.comentarios;
-    })
-})
